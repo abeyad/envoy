af3427265b817a1fa7707e40803bf653e2d3ac41
Ali Beyad <abeyad@google.com>
2022-12-09 11:19:43 -0500

    stuff
    Signed-off-by: Ali Beyad <abeyad@google.com>

--- mobile/library/cc/envoy_error.h
+++ mobile/library/cc/envoy_error.h
@@ -12,14 +12,13 @@ namespace Envoy {
 namespace Platform {
 
 struct EnvoyError {
-  envoy_error_code_t error_code;
+  envoy_error_code_t error_code; 
   std::string message;
   absl::optional<int> attempt_count;
   absl::optional<std::exception> cause;
 };
 
 using EnvoyErrorSharedPtr = std::shared_ptr<EnvoyError>;
-                          
 
 } // namespace Platform
 } // namespace Envoy
